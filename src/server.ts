d3b368414e94d43ceac57e30af98ce74d8812798
Iago <iagobrazmendes@gmail.com>
2020-11-03 09:32:11 -0300

    connect to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect('mongodb://localhost/stemguy', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection
+.once('open', () => console.log('database connected'))
+.on('error', error => console.log('[database connection error]:', error))
 
 app.listen(3333, () => console.log('server started'))
