bd1f034d8c8c90f6741851285f6bc602f02ca6db
Iago <iagobrazmendes@gmail.com>
2020-11-10 19:51:42 -0300

    handle upload images in author

--- src/controllers/Author.ts
+++ src/controllers/Author.ts
@@ -1,4 +1,6 @@
 import {Request, Response} from 'express'
+import fs from 'fs'
+import path from 'path'
 
 import Author from '../models/Author'
 
@@ -6,7 +8,12 @@ export default
 {
     async create(req: Request, res: Response)
     {
-        const {name, role, image} = req.body
+        const {name, role} = req.body
+        const upload = req.file
+
+        let image
+        if (upload) image = upload.filename
+        
         const author = await Author.create({name, role, image})
         return res.status(201).json(author)
     },
@@ -14,7 +21,22 @@ export default
     async update(req: Request, res: Response)
     {
         const {id} = req.params
-        const {name, role, image} = req.body
+        const {name, role} = req.body
+        const upload = req.file
+
+        let image
+        const previous = await Author.findById(id)
+        if (previous)
+        {
+            if (upload && previous.image)
+            {
+                fs.unlinkSync(path.join(__dirname, '..', '..', 'uploads', previous.image))
+                image = upload.filename
+            }
+            else if (upload) image = upload.filename
+            else if (previous.image) image = previous.image
+        }
+
         const author = await Author.findByIdAndUpdate(id, {name, role, image}, {new: true})
         return res.status(200).json(author)
     },
